219c36101cc85f743616629ef785510af2fb4b6d
Arie Lakeman <arie.lakeman@boom.co>
2024-10-02 19:26:09 +0100

    nice but messy 3d blob

--- site/src/App.js
+++ site/src/App.js
@@ -3,7 +3,63 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+
+import { OrthographicCamera } from '@react-three/drei'
+import * as THREE from 'three'
+import { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { MeshDistortMaterial, GradientTexture, useCursor } from '@react-three/drei'
+
+
 import { HomePage } from "./pages/Home";
+
+import { useThree } from '@react-three/fiber'
+
+function Box() {
+  const { viewport } = useThree()
+  const ref = useRef()
+  useFrame(() => {
+    ref.current.distort = THREE.MathUtils.lerp(ref.current.distort, Math.random() * 0.5, 0.01)
+  }, [])
+  
+  // Place the box in the top-right corner
+  return (
+    <mesh  rotation={[-Math.PI / 10, Math.PI / 4, 0]} position={[viewport.width / 2 - 1, viewport.height / 2 - 1, 0]}>
+      <sphereGeometry rotateY={0.5} args={[0.8, 64, 64]} />
+      {/*<meshStandardMaterial color="skyblue" />*/}
+      <MeshDistortMaterial ref={ref} speed={5}>
+        <GradientTexture stops={[0, 0.3, 0.6, 0.9, 1]} colors={['#B0E0B9', '#7b8db0', '#a1b1cf', '#ffffff']} size={100} />
+      </MeshDistortMaterial>
+    </mesh>
+  )
+}
+
+function MovingPointLight() {
+  const lightRef = useRef()
+
+  // Animate the light's position in a circular motion
+  useFrame(({ clock }) => {
+    const time = clock.getElapsedTime()
+    const radius = 0.5 // Radius of the circle the light moves on
+    if (lightRef.current) {
+      lightRef.current.position.x = radius * Math.sin(time / 5)
+    }
+  })
+
+  return <pointLight ref={lightRef} position={[0, 1, 1]} intensity={1} />
+}
+
+function Scene3D() {
+
+  return (
+    <Canvas style={{ opacity: 0.98, position: 'absolute', top: 20, right: 20, zIndex: 1, width: 300, height: 300 }}>
+      <ambientLight intensity={5} />
+      <MovingPointLight />
+      <Box />
+      <OrthographicCamera makeDefault zoom={100} position={[0, 0, 5]} />
+    </Canvas>
+  )
+}
 
 const ErrorPage = ({ text = "Page Not Found" }) => {
   return <div>
@@ -72,6 +128,7 @@ const router = createBrowserRouter([
 function App() {
   return (
     <>
+      <Scene3D />
       <div className="content-area">
         <div className="details">
           <img src="/blocks.svg" style={{ position: 'absolute', top: -20, left: 20 }} />
